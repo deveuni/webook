b57c2f64cfe8f1e61e1acb594c392b7142d8d2e5
지은 <vividdream0215@naver.com>
2020-12-30 22:06:55 +0900

    MemberVO, GoodsVO 생성

---
+++ src/main/java/com/webook/domain/GoodsVO.java
@@ -0,0 +1,98 @@
+package com.webook.domain;
+
+import java.sql.Timestamp;
+
+public class GoodsVO {
+	
+	private int gdsNum;
+	private String gdsName;
+	private String cateCode;
+	private int gdsPrice;
+	private int gdsStock;
+	private String gdsDes;
+	private String gdsImg;
+	private Timestamp gdsDate;
+
+	
+	// 생성자 
+	public GoodsVO() {}
+	public GoodsVO(int gdsNum, String gdsName, String cateCode, int gdsPrice, int gdsStock, String gdsDes,
+			String gdsImg, Timestamp gdsDate) {
+		super();
+		this.gdsNum = gdsNum;
+		this.gdsName = gdsName;
+		this.cateCode = cateCode;
+		this.gdsPrice = gdsPrice;
+		this.gdsStock = gdsStock;
+		this.gdsDes = gdsDes;
+		this.gdsImg = gdsImg;
+		this.gdsDate = gdsDate;
+	}
+	
+	// set(), get() 메소드
+	public int getGdsNum() {
+		return gdsNum;
+	}
+	public void setGdsNum(int gdsNum) {
+		this.gdsNum = gdsNum;
+	}
+	public String getGdsName() {
+		return gdsName;
+	}
+	public void setGdsName(String gdsName) {
+		this.gdsName = gdsName;
+	}
+	public String getCateCode() {
+		return cateCode;
+	}
+	public void setCateCode(String cateCode) {
+		this.cateCode = cateCode;
+	}
+	public int getGdsPrice() {
+		return gdsPrice;
+	}
+	public void setGdsPrice(int gdsPrice) {
+		this.gdsPrice = gdsPrice;
+	}
+	public int getGdsStock() {
+		return gdsStock;
+	}
+	public void setGdsStock(int gdsStock) {
+		this.gdsStock = gdsStock;
+	}
+	public String getGdsDes() {
+		return gdsDes;
+	}
+	public void setGdsDes(String gdsDes) {
+		this.gdsDes = gdsDes;
+	}
+	public String getGdsImg() {
+		return gdsImg;
+	}
+	public void setGdsImg(String gdsImg) {
+		this.gdsImg = gdsImg;
+	}
+	public Timestamp getGdsDate() {
+		return gdsDate;
+	}
+	public void setGdsDate(Timestamp gdsDate) {
+		this.gdsDate = gdsDate;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "GoodsVO [gdsNum=" + gdsNum + ", gdsName=" + gdsName + ", cateCode=" + cateCode + ", gdsPrice="
+				+ gdsPrice + ", gdsStock=" + gdsStock + ", gdsDes=" + gdsDes + ", gdsImg=" + gdsImg + ", gdsDate="
+				+ gdsDate + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/webook/domain/MemberVO.java
@@ -0,0 +1,101 @@
+package com.webook.domain;
+
+import java.sql.Timestamp;
+
+public class MemberVO {
+	
+	private String userId;
+	private String userPass;
+	private String userName;
+	private String userPhon;
+	private String userEmail;
+	private String userAddr1;
+	private String userAddr2;
+	private String postcode;
+	private Timestamp reg_date;
+	
+	// 생성자
+	public MemberVO() {}
+	public MemberVO(String userId, String userPass, String userName, String userPhon, String userEmail,
+			String userAddr1, String userAddr2, String postcode, Timestamp reg_date) {
+		super();
+		this.userId = userId;
+		this.userPass = userPass;
+		this.userName = userName;
+		this.userPhon = userPhon;
+		this.userEmail = userEmail;
+		this.userAddr1 = userAddr1;
+		this.userAddr2 = userAddr2;
+		this.postcode = postcode;
+		this.reg_date = reg_date;
+	}
+	
+	// set(), get() 메소드
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPass() {
+		return userPass;
+	}
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPhon() {
+		return userPhon;
+	}
+	public void setUserPhon(String userPhon) {
+		this.userPhon = userPhon;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserAddr1() {
+		return userAddr1;
+	}
+	public void setUserAddr1(String userAddr1) {
+		this.userAddr1 = userAddr1;
+	}
+	public String getUserAddr2() {
+		return userAddr2;
+	}
+	public void setUserAddr2(String userAddr2) {
+		this.userAddr2 = userAddr2;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+	
+	
+	// toString()
+	@Override
+	public String toString() {
+		return "MemberVO [userId=" + userId + ", userPass=" + userPass + ", userName=" + userName + ", userPhon="
+				+ userPhon + ", userEmail=" + userEmail + ", userAddr1=" + userAddr1 + ", userAddr2=" + userAddr2
+				+ ", postcode=" + postcode + ", reg_date=" + reg_date + "]";
+	}
+	
+
+}
