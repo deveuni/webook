4e16f6300dfcac1ca6175da29b532d7dd89493d3
지은 <vividdream0215@naver.com>
2021-01-03 22:48:20 +0900

    회원가입 하는 중

--- src/main/java/com/webook/controller/MemberController.java
+++ src/main/java/com/webook/controller/MemberController.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,8 @@ public class MemberController {
 	// 서비스 처리 객체 주입
 	@Inject
 	private MemberService service;
+	
+	
 	
 	private static final Logger log =
 			LoggerFactory.getLogger(MemberController.class);
@@ -40,6 +43,8 @@ public class MemberController {
 		
 		// 뷰페이지에서 전달되는 정보 vo
 		log.info("C : 뷰페이지에서 전달되는 정보 vo => " + vo);
+		
+		// 비밀번호 암호화 - 할까? 찾아보고 정하기
 		
 		// 서비스에서 회원가입 동작 호출
 		service.signup(vo);
