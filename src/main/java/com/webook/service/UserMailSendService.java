d64a6e2440c4e62aabd73076bd40dcda9cb89304
지은 <vividdream0215@naver.com>
2021-01-12 20:37:46 +0900

    회원가입 인증구현중

--- src/main/java/com/webook/service/UserMailSendService.java
+++ src/main/java/com/webook/service/UserMailSendService.java
@@ -1,5 +1,12 @@
 package com.webook.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class UserMailSendService {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
 
 }
