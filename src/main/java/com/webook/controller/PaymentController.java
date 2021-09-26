8fc9fe3c664524b6bbfd553c9102e700b000129b
지은 <vividdream0215@naver.com>
2021-09-26 13:19:30 +0900

    pay

--- src/main/java/com/webook/controller/PaymentController.java
+++ src/main/java/com/webook/controller/PaymentController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.webook.service.PaymentService;
 
@@ -17,5 +18,12 @@ public class PaymentController {
 	@Inject
 	private PaymentService service;
 	
-	private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
+	private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
+	
+	@RequestMapping(value = "/payment", method = RequestMethod.GET)
+	public String detailPayment() throws Exception {
+		
+		return "";
+	}
+	
 }
