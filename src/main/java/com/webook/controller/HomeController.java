be757c7c5a16e8af7815ac6295c3d19d5de03a2c
지은 <vividdream0215@naver.com>
2021-11-02 14:16:03 +0900

    슬라이드

--- src/main/java/com/webook/controller/HomeController.java
+++ src/main/java/com/webook/controller/HomeController.java
@@ -4,12 +4,20 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.webook.domain.Criteria;
+import com.webook.service.MemberService;
+import com.webook.service.ShopService;
 
 /**
  * Handles requests for the application home page.
@@ -22,18 +30,35 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/webook", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	
+	@Inject
+	private MemberService mservice;
+	@Inject
+	private ShopService service;
+	
+	
+	/*
+	 * @RequestMapping(value = "/webook", method = RequestMethod.GET) public String
+	 * home(Locale locale, Model model) {
+	 * logger.info("Welcome home! The client locale is {}.", locale);
+	 * 
+	 * Date date = new Date(); DateFormat dateFormat =
+	 * DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+	 * 
+	 * String formattedDate = dateFormat.format(date);
+	 * 
+	 * model.addAttribute("serverTime", formattedDate );
+	 * 
+	 * 
+	 * return "home"; }
+	 */
+	
+	@RequestMapping(value = "/webook", method = RequestMethod.GET) 
+	public void main(HttpSession session, Model model, @ModelAttribute("cri") Criteria cri) throws Exception {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		// 슬라이드 출력 - 구매수??
+		model.addAttribute("top3goods");
 	}
 	
 }
